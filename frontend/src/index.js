10f98cfc1cc5974d94caa899a0a4164e83d42338
mohamednasser979 <mohamed.nasser979@github.com>
2026-03-07 23:26:59 +0200

    fix bootstrap navbar mobile

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,24 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./index.css";
 import ReactDOM from "react-dom/client";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import "./index.css";
 import App from "./App";
+
 import { AuthProvider } from "./context/AuthContext";
-import { ThemeProvider }from "./context/ThemeContext";
+import { ThemeProvider } from "./context/ThemeContext";
 import { CartProvider } from "./context/CartContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <ThemeProvider>
-    <AuthProvider>
-    <CartProvider>
-      <App />
-      </CartProvider>
-    </AuthProvider>
-  </ThemeProvider>
+<ThemeProvider>
+<AuthProvider>
+<CartProvider>
+<App />
+</CartProvider>
+</AuthProvider>
+</ThemeProvider>
 );
