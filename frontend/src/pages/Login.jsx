738dbc1712af24aa256adadd4c53d1ee510cb59a
mohamednasser979 <mohamed.nasser979@github.com>
2026-03-07 21:21:18 +0200

    fix login api url

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -19,7 +19,7 @@ function Login() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://ghazl-fashion-production.up.railway.app/api/auth/login",
         form
       );
 
