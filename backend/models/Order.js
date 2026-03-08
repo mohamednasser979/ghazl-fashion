5a635ffb007db8cb935ecb63042484451c9d51d0
mohamednasser979 <mohamed.nasser979@github.com>
2026-03-08 02:24:58 +0200

    fix order schema

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,50 +1,41 @@
 const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema(
-  {
+const orderSchema = new mongoose.Schema({
 
-    items: [
-      {
-        name: String,
-        price: Number,
-        qty: Number,
-        image: String,
-        size: String,
-        color: String
-      }
-    ],
-
-    totalPrice: {
-      type: Number,
-      required: true
-    },
-
-    shippingAddress: {
-      fullName: String,
-      phone: String,
-      address: String,
-      city: String
-    },
-
-    status:{
-      type:String,
-      default:"pending"
-    },
-
-    paymentStatus:{
-      type:String,
-      enum:["pending","paid","failed"],
-      default:"pending"
-    },
-
-    orderStatus:{
-      type:String,
-      enum:["processing","shipped","delivered"],
-      default:"processing"
-    }
-
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
   },
-  { timestamps:true }
-);
 
-module.exports = mongoose.model("Order",orderSchema);
+  items: [
+    {
+      name: String,
+      price: Number,
+      qty: Number,
+      image: String,
+      size: String,
+      color: String
+    }
+  ],
+
+  totalPrice: {
+    type: Number,
+    required: true
+  },
+
+  shippingAddress: {
+    fullName: String,
+    phone: String,
+    address: String,
+    city: String
+  },
+
+  orderStatus: {
+    type: String,
+    default: "Pending"
+  }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model("Order", orderSchema);
